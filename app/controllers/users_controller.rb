410ff233f6cbea757a96040b73dff397e0d04f05
shirohige3 <shirohigesan08@gmail.com>
2020-09-23 14:50:37 +0900

    ログイン・ログアウト機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      return redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_date)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
          has_many :itemn_customers
          has_many :items, through:  :itemn_customers
 
-         validates :email.match(/@+/), presence: true
+         validates :email, presence: true, format:{ with: /@+/, message: '@マークを含めたアドレスを入力してください' }
 
-         validates :password.match(/[a-z\d]{8,}/i), presence: true 
+         validates :password, presence: true, format:{ with: /[a-z\d]{8,}/i, message: '半角英数字で６文字以上で入力してください' }
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字で入力してください' } do
           validates :family_name
           validates :first_name
          end
 
-         with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+         with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字で入力して下さい' } do
           validates :family_name_kana
           validates :first_name_kana
          end
