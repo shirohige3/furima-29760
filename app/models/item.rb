ac0b22b9dd19217c3438d27bcd900a00a375d5c5
shirohige3 <shirohigesan08@gmail.com>
2020-09-25 19:17:58 +0900

    activehashで販売価格以外実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :ship_form
+  belongs_to_active_hash :shipping_day
+
 has_one          :item_customer
 belongs_to       :user
 has_one_attached :image
@@ -12,13 +19,14 @@ validates :description,       presence: true, length: { maximum: 1000 }
 # validates :price,             presence: true, format: { with: /\A30[0-9]|99999999[0-9]+\z/i, message: '半角数字で¥300〜¥9,999,999の間で入力してください'}
 validates :price,             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
-validates :category_id,       presence: true
+validates :category, presence: true
+validates :category_id,       numericality: { other_than: 1 } 
 
-validates :condition_id,      presence: true
+validates :condition_id,      numericality: { other_than: 1 } 
 
-validates :shipping_fee_id,  presence: true
+validates :shipping_fee_id,  numericality: { other_than: 1 } 
 
-validates :ship_form_id,      presence: true
+validates :ship_form_id,      numericality: { other_than: 1 } 
 
-validates :shipping_day_id,   presence: true
+validates :shipping_day_id,   numericality: { other_than: 1 } 
 end
