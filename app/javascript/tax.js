2e90581dcf530a4f00c56f5e32b5cf9d4587b3dc
shirohige3 <shirohigesan08@gmail.com>
2020-09-27 16:22:52 +0900

    前回のtax.jsのイベントハンドラ修正

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -2,7 +2,8 @@ function priceCalc(){
   let itemPrice = document.getElementById("item-price");
   let addTaxPrice = document.getElementById("add-tax-price");
   let profit = document.getElementById("profit");
-  itemPrice.addEventListener("change",()=>{
+  itemPrice.addEventListener("keypress",()=>{
+    console.log("test")
     let priceValue = itemPrice.value;
     let taxPrice = Math.round(priceValue * 0.1)
     addTaxPrice.innerHTML = taxPrice
