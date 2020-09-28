11e41649104192891f78974fad4c9b9ec0a0dc58
shirohige3 <shirohigesan08@gmail.com>
2020-09-28 21:54:48 +0900

    destroyアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def update
