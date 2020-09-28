5e402b4cedab104e81480883a1e012b8bf920708
shirohige3 <shirohigesan08@gmail.com>
2020-09-28 11:36:08 +0900

    詳細ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,9 +21,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :category_id, :condition_id, :shipping_fee_id, :ship_form_id, :shipping_day_id).merge(user_id: current_user.id)
   end
+
 end
