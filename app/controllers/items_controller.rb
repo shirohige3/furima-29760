7296c4b268905250d395ea3d501aa5789f143da5
shirohige3 <shirohigesan08@gmail.com>
2020-09-28 21:50:10 +0900

    削除機能削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,11 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      reder :show
-    end
   end
 
   def update
